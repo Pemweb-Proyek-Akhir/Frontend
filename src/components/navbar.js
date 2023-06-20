aa8c4368659a002dee689b6cc8479ee3ff68994b
mfadlihs <winnerfadli999@gmail.com>
2023-06-20 23:01:18 +0700

    jumbotron

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,23 @@
 export default function Navbar() {
   return (
-    <div className="p-5 shadow flex justify-between items-center">
-      <div className="[&>div:hover]:text-primary [&>div:hover]:cursor-pointer flex space-x-7 text-base font-medium items-center">
-        <img src="/logo/logo-blue.svg" />
-        <div className="hover:text-primary">Company</div>
-        <div>Marketplace</div>
-        <div>Features</div>
-        <div>Team</div>
-        <div>Contact</div>
+    <>
+      <div className="p-5 shadow-lg bg-white flex justify-between items-center fixed top-0 z-10 w-screen">
+        <div className="[&>div:hover]:text-primary [&>div:hover]:cursor-pointer flex space-x-7 text-base font-medium items-center">
+          <img src="/logo/logo-blue.svg" />
+          <div className="hover:text-primary">Company</div>
+          <div>Marketplace</div>
+          <div>Features</div>
+          <div>Team</div>
+          <div>Contact</div>
+        </div>
+        <div className="flex space-x-6 items-center">
+          <button className="px-4 py-2 rounded-md shadow-md">Login</button>
+          <button className="bg-primary text-white font-medium px-3 py-2 rounded-md  block">
+            Register
+          </button>
+        </div>
       </div>
-      <div className="flex space-x-6 items-center">
-        <button className="px-4 py-2 rounded-md shadow-md">Login</button>
-        <button className="bg-primary text-white font-medium px-3 py-2 rounded-md  block">
-          Register
-        </button>
-      </div>
-    </div>
+      <div className="h-[88px]"></div>
+    </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,22 @@ export default function Home() {
   return (
     <div>
       <Navbar />
+      <div className="relative">
+        <div className="j2">
+          <div className="w-[35%] pl-12 h-full flex justify-center flex-col">
+            <div className="font-bold text-white text-5xl mb-6">
+              Dari Nasi Uduk sampai Mie Gludug
+            </div>
+            <div className="font-medium text-white text-2xl mb-5">
+              Pesennya sat set tinggal duduk Makan puas ampe perut hampir
+              meledug
+            </div>
+            <img src="/logo/logo-white.svg" width={"248px"} />
+          </div>
+        </div>
+        <div className="j1 absolute"></div>
+        <img className="absolute banner" src="/images/jumbotron.jpg" />
+      </div>
     </div>
   );
 }
