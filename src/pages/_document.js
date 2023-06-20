fd5c001ee6e9b39a753d51c374a9d9164d27a3e5
mfadlihs <winnerfadli999@gmail.com>
2023-06-20 22:32:40 +0700

    change logo and title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Aseupan Kitchen</title>
+        <link rel="icon" href="/logo.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
